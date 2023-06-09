2491fb5efdad3fa72f639f97298679a4396ea89d
Yash Shivhare <102593040+YashShivhare007@users.noreply.github.com>
2023-06-10 02:27:48 +0530

    Add files via upload

---
+++ user/wc.d
@@ -0,0 +1,2 @@
+user/wc.o: user/wc.c kernel/types.h kernel/stat.h kernel/types.h \
+ user/user.h
---
+++ user/zombie.d
@@ -0,0 +1,2 @@
+user/zombie.o: user/zombie.c kernel/types.h kernel/stat.h kernel/types.h \
+ user/user.h
